b0828864b9a873bf917c65b83ec33512919ebe19
amandeep0511 <amandeepstj@gmail.com>
2019-04-02 20:07:30 +0530

    Add GET/ todos route

--- server/server.js
+++ server/server.js
@@ -17,6 +17,15 @@ app.post('/todos',(req,res)=>{
     res.status(400).send('Bad request');
   })
 });
+
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos})
+  },(e)=>{
+    res.status(400).send(e);
+  })
+})
+
 app.listen(3000,()=>{
   console.log('Connected to 3000 port');
 });
