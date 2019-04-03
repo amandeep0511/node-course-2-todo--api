6746809569c0b44c49abe0702f04d6c57566364e
amandeep0511 <amandeepstj@gmail.com>
2019-04-03 18:42:24 +0530

    Add get todos/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser  = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./model/todo');
 var {User} = require('./model/user')
@@ -24,7 +25,23 @@ app.get('/todos',(req,res)=>{
   },(e)=>{
     res.status(400).send(e);
   })
-})
+});
+
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+});
 
 app.listen(3000,()=>{
   console.log('Connected to 3000 port');
