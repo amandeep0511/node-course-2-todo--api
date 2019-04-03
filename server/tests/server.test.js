a3df03136f834533882ded4cd6d7f64bfa4812bf
amandeep0511 <amandeepstj@gmail.com>
2019-04-03 18:57:25 +0530

    Added test cases for GET /todos/id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,12 +3,15 @@ const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../model/todo');
+const {ObjectID} = require('mongodb');
 
 const todos = [{
+  _id: new ObjectID(),
   text: "Testing 1"
 },{
+  _id: new ObjectID(),
   text: "Testing 2"
-}]
+}];
 
 beforeEach((done)=>{
   Todo.remove({})
@@ -72,4 +75,31 @@ describe('GET /todos',()=>{
           })
           .end(done);
   });
+});
+
+describe('GET todos/:id',()=>{
+  it('should return todo doc',(done)=>{
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found',(done)=>{
+    var id =new ObjectID();
+    request(app)
+      .get(`/todos/${id.toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object id',(done)=>{
+    request(app)
+      .get(`/todos/123`)
+      .expect(404)
+      .end(done);
+  })
 });
